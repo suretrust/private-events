2055e4bbdba5b5c4577fea852fdeb8bd243da18f
Saheed <sdrfngg640@gmail.com>
2019-05-23 11:14:26 +0100

    Change root URL

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :events, only: [:new, :create, :show, :index]
   resources :users, only: %i[new create show]
   resource :session, only: %i[new create destroy]
-  root 'users#new'
+
+  root 'events#index'
 end
