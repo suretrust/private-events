fe53e5d3ec2d590161df0f2966b20fc9b3ca6f1c
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-05-27 22:00:09 +0200

    redirect user to events_path if he deliberately goes to sign-up or sign-in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  before_action :redirect_to_home_if_already_signed_in, only: [:new]
+  
   def new; end
 
   def create
@@ -26,4 +28,11 @@ class SessionsController < ApplicationController
       redirect_to new_session_path
     end
   end
+
+  private
+
+  def redirect_to_home_if_already_signed_in
+    redirect_to events_path if logged_in?
+  end
+  
 end
